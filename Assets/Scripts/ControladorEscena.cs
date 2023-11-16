be863d6ab08668e9e31751d7ff96696b5ee39f8e
Miguel <m.carmonag.2020@alumnos.urjc.es>
2023-11-16 03:01:13 +0100

    Diseño de niveles
    Por problemas de la rama "diseñoNiveles" se hace commit desde el main del contenido de dicha rama.
    Añadidos 6 niveles: Cada nivel es diferente e incorpora nuevos enemigos.
    - Añadidas estructura de suelo, niebla y obstáculos.
    - Añadidos diferentes enemigos por nivel.
    - Añadidos ítems obtenibles en cada nivel.
    Reestructuración de escenas.
    Añadido menú para nueva partida:
    - Añadida opción de jugar tutorial o no jugarlo.

--- Assets/Scripts/ControladorEscena.cs
+++ Assets/Scripts/ControladorEscena.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class ControladorEscena : MonoBehaviour
 {
+    public GameObject tutorial;
+
     public void CambiarEscena(string nombre)
     {
         SceneManager.LoadScene(nombre);
+    }
+
+    public void PopUp()
+    {
+        if (!tutorial.activeSelf)
+            tutorial.SetActive(true);
+        else
+            tutorial.SetActive(false);
     }
 
     public void CerrarJuego()
