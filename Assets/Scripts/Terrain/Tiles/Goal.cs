2c43df94317265bc58e646a581ebd39aac8b25b0
Rodro421 <r.hervada.2020@alumnos.urjc.es>
2023-11-19 03:16:35 +0100

    Interacción de historia con piezas y carteles
    Interacción de historia con piezas y carteles

--- Assets/Scripts/Terrain/Tiles/Goal.cs
+++ Assets/Scripts/Terrain/Tiles/Goal.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         gameObject.GetComponent<Renderer>().material.color = new Color32(255, 0, 232, 255);
     }
